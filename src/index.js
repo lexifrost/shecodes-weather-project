6732c92cd761029d383cae299ecffc6494b0394a
lexifrost <alexa.frost@outlook.com>
2024-04-13 15:50:08 +0800

    Injected weather icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
@@ -14,6 +15,10 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  icon.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="temperature-icon"
+              />`;
 }
 
 function formatDate(date) {
